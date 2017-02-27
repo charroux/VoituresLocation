efee242f7d312e508283a99125ea7bf0187f6bb7
charroux <benoit.charroux@free.fr>
2017-02-27 13:14:21 +0100

    adding put, post, delete

--- src/main/java/com/descartes/web/ControllerLocation.java
+++ src/main/java/com/descartes/web/ControllerLocation.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,6 +26,36 @@ public class ControllerLocation {
 	@ResponseBody
 	@ResponseStatus(HttpStatus.OK)
 	public List<Voiture> getListeVoiture(){		
+		System.out.println("getListeVoiture()");
 		return voitures;
 	}
+
+	@RequestMapping(value="/voiture", method=RequestMethod.POST)
+	@ResponseStatus(HttpStatus.OK)
+	public void addVoitureParBodyHTTP(@RequestBody Voiture voiture){
+		System.out.println("addVoitureParBodyHTTP : " + voiture);
+		voitures.add(voiture);
+	}
+	
+	@RequestMapping(value="/voiture", method=RequestMethod.PUT)
+	@ResponseStatus(HttpStatus.OK)
+	public void modifierVoiture( @RequestBody Voiture voiture){
+		System.out.println("modifierVoiture : " + voiture);
+		int index = voitures.indexOf(voiture);
+		if(index != -1){
+			voitures.remove(index);
+			voiture.setImmatriculation("xxxxx");
+			voitures.add(voiture);
+		}
+	}
+	
+	@RequestMapping(value="/voiture", method=RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.OK)
+	public void deleteVoiture( @RequestBody Voiture voiture){
+		System.out.println("deleteVoiture : " + voiture);
+		voitures.remove(voiture);
+	}
+	
+
+
 }
--- src/main/java/com/descartes/web/Voiture.java
+++ src/main/java/com/descartes/web/Voiture.java
@@ -31,5 +31,37 @@ public class Voiture {
 		this.marque = marque;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((immatriculation == null) ? 0 : immatriculation.hashCode());
+		result = prime * result + ((marque == null) ? 0 : marque.hashCode());
+		return result;
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Voiture other = (Voiture) obj;
+		if (immatriculation == null) {
+			if (other.immatriculation != null)
+				return false;
+		} else if (!immatriculation.equals(other.immatriculation))
+			return false;
+		if (marque == null) {
+			if (other.marque != null)
+				return false;
+		} else if (!marque.equals(other.marque))
+			return false;
+		return true;
+	}
+
+	
 }
